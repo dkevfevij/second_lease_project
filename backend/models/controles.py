c36d3e6c323333a9dbbd2b35fd5db98dbe2b2cc7
Nisrine Hilali <nisrinhilali2003@gmail.com>
2025-07-25 11:26:03 +0100

    etat pret a livrer

--- backend/models/controles.py
+++ backend/models/controles.py
@@ -1,6 +1,4 @@
-# backend/models/controles.py
-
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from .database import Base
 
 class Controle(Base):
@@ -8,6 +6,11 @@ class Controle(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     camion_id = Column(Integer, ForeignKey("camions.id"), nullable=False)
-    type = Column(String, nullable=False)  # Ex : test batterie, contrôle visuel
+    type = Column(String, nullable=False)  # Ex : test_batterie, controle_visuel, demarrage
     resultat = Column(String)
     date_controle = Column(DateTime)
+
+    # Champs à ajouter :
+    is_reminder = Column(Boolean, default=False)  # True si c’est un contrôle périodique
+    valide = Column(Boolean, default=False)       # True si validé manuellement
+    count = Column(Integer, default=1)            # Nombre de fois effectué
